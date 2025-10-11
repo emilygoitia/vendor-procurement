296bc46b302109c7bc6a002fd6cf07c09d84d445
emilygoitia <emily@manocg.com>
2025-10-11 20:00:44 +0200

    feat: extend scheduling for construction and rfp closure

--- components/slider.py
+++ components/slider.py
@@ -1,7 +1,26 @@
 import streamlit as st
 
-def render_styled_slider(label, min_value, max_value, default_value, help=None):
+
+def render_styled_slider(
+    label,
+    min_value,
+    max_value,
+    default_value,
+    step=1,
+    help=None,
+    key=None,
+    disabled=False,
+):
     with st.container():
         st.write("<div class='styled-slider' />", unsafe_allow_html=True)
-        slider = st.slider(label, min_value, max_value, default_value, help=help)
+        slider = st.slider(
+            label,
+            min_value=min_value,
+            max_value=max_value,
+            value=default_value,
+            step=step,
+            help=help,
+            key=key,
+            disabled=disabled,
+        )
         return slider
