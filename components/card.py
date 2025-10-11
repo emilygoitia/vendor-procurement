9bfcc547fc5b5ab574d6646e41af83b585dcf6cc
emilygoitia <emily@manocg.com>
2025-10-11 21:36:08 +0200

    Remove vendor name header from KPI cards

--- components/card.py
+++ components/card.py
@@ -1,4 +1,4 @@
 import utils.colors as colors
 
 def render_kpi_card(building, label, key):
-  return f"<div class=\"kpi-card\"><div><p>{building['building_name']}</p><span class='label'>{label}</span></div><span class='date'>{building[key]}</span></div>"
+  return f"<div class=\"kpi-card\"><div><span class='label'>{label}</span></div><span class='date'>{building[key]}</span></div>"
